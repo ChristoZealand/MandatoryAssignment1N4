2c1e97cab8ce8e7230c597d5b51e5c669480d4ab
ChristoZealand <102149012+ChristoZealand@users.noreply.github.com>
2022-10-04 01:43:28 +0200

    Add files via upload

---
+++ FootballPlayersController.cs
@@ -0,0 +1,63 @@
+﻿using MandatoryAssignment1;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MandatoryAssignment4
+{
+    [Route("api/FootballPlayers")]
+    [ApiController]
+    public class FootballPlayersController : ControllerBase
+    {
+        private readonly FootballPlayersManager _footballPlayersManager = new FootballPlayersManager();
+
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [HttpGet()]
+        public ActionResult<IEnumerable<FootballPlayer>> Get()
+        {
+            IEnumerable<FootballPlayer> player = _footballPlayersManager.GetAll();
+
+            if (player.Count() == 0) return NotFound("No such class, id");
+            return Ok(player);
+        }
+
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [HttpGet("{id}")]
+        public ActionResult<FootballPlayer> Get(int id)
+        {
+            FootballPlayer player = _footballPlayersManager.GetById(id);
+            if (player == null) return NotFound("No such class, id: " + id);
+            return Ok(player);
+        }
+
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [HttpPost]
+        public ActionResult<FootballPlayer> Post([FromBody] FootballPlayer value)
+        {
+            FootballPlayer player = _footballPlayersManager.Add(value);
+            if (player == value) return Created("Api/IPAs/" + value.Id, value);
+            return Created("", player);
+        }
+
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [HttpPut("{id}")]
+        public ActionResult<FootballPlayer> Put(int id, [FromBody] FootballPlayer value)
+        {
+            FootballPlayer player = _footballPlayersManager.Update(id, value);
+            if (player.ShirtNumber > 99) return BadRequest("BAAAD");
+            return BadRequest();
+        }
+
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [HttpDelete("{id}")]
+        public ActionResult<FootballPlayer> Delete(int id)
+        {
+            FootballPlayer player = _footballPlayersManager.Delete(id);
+            if (player == null) return NoContent();
+            return NoContent();
+
+        }
+    }
+}
---
+++ FootballPlayersManager.cs
@@ -0,0 +1,62 @@
+﻿using System.Reflection;
+using MandatoryAssignment1;
+
+namespace MandatoryAssignment4
+{
+    public class FootballPlayersManager
+    {
+        private static int nextid = 1;
+
+        private static readonly List<FootballPlayer> footballPlayers = new List<FootballPlayer>
+        {
+            new FootballPlayer{Id = nextid++, Name = "Kennedy", Age = 25, ShirtNumber = 78},
+            new FootballPlayer{Id = nextid++, Name = "Flora", Age = 29, ShirtNumber = 23},
+            new FootballPlayer{Id = nextid++, Name = "Kevin", Age = 34, ShirtNumber = 55},
+            new FootballPlayer{Id = nextid++, Name = "Mick", Age = 19, ShirtNumber = 99}
+        };
+
+        public FootballPlayersManager()
+        {
+            
+        }
+
+        // GetAll, GetById, Add, Update and Delete
+
+        public List<FootballPlayer> GetAll()
+        {
+            return new List<FootballPlayer>(footballPlayers);
+        }
+
+        public FootballPlayer GetById(int? id)
+        {
+            return footballPlayers.Find(footballplayer => footballplayer.Id == id);
+        }
+
+        public FootballPlayer Add(FootballPlayer newPlayer)
+        {
+            newPlayer.Id = nextid++;
+            footballPlayers.Add(newPlayer);
+            return newPlayer;
+        }
+
+        public FootballPlayer Delete(int id)
+        {
+            FootballPlayer deletePlayer = footballPlayers.Find(footballplayer => footballplayer.Id == id);
+            if (deletePlayer == null) return null;
+            footballPlayers.Remove(deletePlayer);
+            return deletePlayer;
+        }
+
+        public FootballPlayer Update(int id, FootballPlayer update)
+        {
+            FootballPlayer updatePlayer = footballPlayers.Find(footballplayer => footballplayer.Id == id);
+            if (updatePlayer == null) return null;
+            updatePlayer.Id = update.Id;
+            updatePlayer.Name = update.Name;
+            updatePlayer.Age = update.Age;
+            updatePlayer.ShirtNumber = update.ShirtNumber;
+
+            return updatePlayer;
+        }
+    }
+}
