0363afb03a9370ea755b57ad6039ca1d03be6e54
ChristoZealand <102149012+ChristoZealand@users.noreply.github.com>
2022-10-04 01:42:17 +0200

    Add files via upload

---
+++ MandatoryAssignment1/MandatoryAssignment1/FootballPlayer.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MandatoryAssignment1
+{
+    public class FootballPlayer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public int ShirtNumber { get; set; }
+
+        public FootballPlayer()
+        {
+                
+        }
+
+        public FootballPlayer(int id, string name, int age, int shirtNumber)
+        {
+            Id = id;
+            Name = name;
+            Age = age;
+            ShirtNumber = shirtNumber;
+        }
+
+        public void ValidateName()
+        {
+            if (Name.Length < 2)
+            {
+                throw new ArgumentOutOfRangeException("Name has to be at least two characters!");
+            }
+        }
+
+        public void ValidateAge()
+        {
+            if (Age < 1)
+            {
+                throw new ArgumentOutOfRangeException("Age has to be greater than 1!");
+            }
+        }
+
+        public void ValidateShirtNumber()
+        {
+            if (ShirtNumber < 1 || ShirtNumber > 99)
+            {
+                throw new ArgumentOutOfRangeException("Shirtnumber has to be between 1 and 99!");
+            }
+        }
+
+        public void Validate()
+        {
+            ValidateName();
+            ValidateAge();
+            ValidateShirtNumber();
+        }
+        public override string ToString()
+        {
+            return $"{{{nameof(Id)}={Id.ToString()}, {nameof(Name)}={Name}, {nameof(Age)}={Age.ToString()}, {nameof(ShirtNumber)}={ShirtNumber.ToString()}}}";
+        }
+    }
+}
---
+++ MandatoryAssignment1/MandatoryAssignment1Tests/FootballPlayerTests.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MandatoryAssignment1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MandatoryAssignment1.Tests
+{
+    [TestClass()]
+    public class FootballPlayerTests
+    {
+        FootballPlayer acceptablePlayer = new FootballPlayer{Id = 1, Name = "Barry", Age = 18, ShirtNumber = 55};
+        FootballPlayer namePlayer = new FootballPlayer{Id = 2,Name = "A", Age = 30, ShirtNumber = 65};
+        FootballPlayer agePlayer = new FootballPlayer{Id = 3, Name = "Lauren", Age = 0, ShirtNumber = 77};
+        FootballPlayer shirtNumberPlayer = new FootballPlayer{Id = 4, Name = "Tiffany", Age = 25, ShirtNumber = 100};
+        FootballPlayer shirtNumberPlayer2 = new FootballPlayer{Id = 5, Name = "Kevin", Age = 86, ShirtNumber = 0};
+
+
+        [TestMethod()]
+        public void ValidateToStringTest()
+        {
+            string str = acceptablePlayer.ToString();
+            Assert.AreEqual("{Id=1, Name=Barry, Age=18, ShirtNumber=55}", str);
+        }
+
+        [TestMethod()]
+        public void ValidateNameTest()
+        {
+            acceptablePlayer.ValidateName();
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => namePlayer.ValidateName());
+        }
+
+        [TestMethod()]
+        public void ValidateAgeTest()
+        {
+            acceptablePlayer.ValidateAge();
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => agePlayer.ValidateAge());
+        }
+
+        [TestMethod()]
+        public void ValidateShirtNumberTest()
+        {
+            acceptablePlayer.ValidateShirtNumber();
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => shirtNumberPlayer.ValidateShirtNumber());
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => shirtNumberPlayer2.ValidateShirtNumber());
+        }
+
+        [TestMethod()]
+        public void ValidateTest()
+        {
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => namePlayer.Validate());
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => agePlayer.Validate());
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => shirtNumberPlayer.Validate());
+        }
+    }
+}
